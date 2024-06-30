f572330a0effe5fb7a6325aecaae66a4a528a83b
daniyalahmed <daniyalaq111@gmail.com>
2024-06-30 23:36:16 +0500

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using AspNetIdentityWithExistingProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AspNetIdentityWithExistingProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            //Create an Instance of the ApplicationDbContext class
+            ApplicationDbContext ctx = new ApplicationDbContext();
+            //Fetch all the users, this will create the database behind the scene
+            List<ApplicationUser> users = ctx.Users.ToList();
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
